96347addc9e27ad93662e39625c6e1b59f2d02cd
Mariia Kravtsova <mariia@quikly.com>
2018-02-24 22:07:01 -0500

    Start new pebble project

---
+++ countdown/build/chalk/appinfo.auto.c
@@ -0,0 +1,20 @@
+#include "pebble_process_info.h"
+#include "src/resource_ids.auto.h"
+
+const PebbleProcessInfo __pbl_app_info __attribute__ ((section (".pbl_header"))) = {
+  .header = "PBLAPP",
+  .struct_version = { PROCESS_INFO_CURRENT_STRUCT_VERSION_MAJOR, PROCESS_INFO_CURRENT_STRUCT_VERSION_MINOR },
+  .sdk_version = { PROCESS_INFO_CURRENT_SDK_VERSION_MAJOR, PROCESS_INFO_CURRENT_SDK_VERSION_MINOR },
+  .process_version = { 1, 0 },
+  .load_size = 0xb6b6,
+  .offset = 0xb6b6b6b6,
+  .crc = 0xb6b6b6b6,
+  .name = "countdown",
+  .company = "MakeAwesomeHappen",
+  .icon_resource_id = DEFAULT_MENU_ICON,
+  .sym_table_addr = 0xA7A7A7A7,
+  .flags = PROCESS_INFO_PLATFORM_CHALK,
+  .num_reloc_entries = 0xdeadcafe,
+  .uuid = { 0x28, 0xFD, 0x50, 0x48, 0xFA, 0x88, 0x4D, 0xB0, 0x8D, 0x1F, 0x95, 0xA1, 0x5D, 0x47, 0x7C, 0x43 },
+  .virtual_size = 0xb6b6
+};
